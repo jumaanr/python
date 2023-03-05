c6776fec3d31d1979f8e723807a39bd04b7042d0
jumaanr <jumaanr@outlook.com>
2023-03-05 18:41:52 +1100

    python basics file created

---
+++ init.py
@@ -0,0 +1,19 @@
+#print something out
+print("Hello World")
+
+#Python Internals
+"""
+Python is an interpreted langauage. "Python interpreter" takes our human friendly python code and parses it line by line.
+Then it creates a machine friendly code which your own computer can understand and execute
+
+The current python 3.0 we use is called as "CPython" which is built out of C Programming lanugage
+Other varients
+
+Cython - Directly written to C limiting the execution time and make it efficient
+Jython - Written in Java
+RPython - mainly used by developers who develop python itself
+
+"""
+#Python basics
+
+
